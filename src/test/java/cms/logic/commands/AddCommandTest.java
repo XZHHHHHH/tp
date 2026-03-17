e4ced679a67da7f3482254cc400b7836e80a95a9
YeeShin504 <58208381+YeeShin504@users.noreply.github.com>
2026-03-17 22:25:01 +0800

    Add tests to check duplicates in conflicting fields for AddCommand

--- src/test/java/cms/logic/commands/AddCommandTest.java
+++ src/test/java/cms/logic/commands/AddCommandTest.java
@@ -54,6 +54,18 @@ public class AddCommandTest {
     }
 
     @Test
+    public void execute_duplicateFields_throwsCommandException() {
+        Person validPerson = new PersonBuilder().build();
+        Person editedPerson = new PersonBuilder(validPerson)
+                .withNusId("A7654321Z")
+                .build();
+        AddCommand addCommand = new AddCommand(editedPerson);
+        ModelStub modelStub = new ModelStubWithPerson(validPerson);
+
+        assertThrows(CommandException.class, AddCommand.MESSAGE_DUPLICATE_FIELDS, () -> addCommand.execute(modelStub));
+    }
+
+    @Test
     public void equals() {
         Person alice = new PersonBuilder().withName("Alice").build();
         Person bob = new PersonBuilder().withName("Bob").build();
@@ -179,6 +191,12 @@ public class AddCommandTest {
         public boolean hasPerson(Person person) {
             requireNonNull(person);
             return this.person.isSamePerson(person);
+        }
+
+        @Override
+        public boolean hasPersonWithConflictingField(Person person) {
+            requireNonNull(person);
+            return this.person.findConflictingField(person) != null;
         }
     }
 
